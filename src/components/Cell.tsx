e187e2676c12b74a8f4f4e589c1e27b25c7a86de
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2024-09-26 20:11:02 +0530

    arrow and star icon added

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,14 +1,40 @@
-import React, {memo} from 'react';
-import {Text, View} from 'react-native';
+import React, { memo, useMemo } from 'react';
+import { View } from 'react-native';
+import { ArrowRightIcon, StarIcon } from 'react-native-heroicons/outline';
+import { RFValue } from 'react-native-responsive-fontsize';
 import tw from 'twrnc';
-import {Colors} from '../constants/Colors';
-import Pile from './Pile';
+import { ArrowSpot, SafeSpots, StarSpots } from '../helpers/PlotData';
 
 const Cell = ({color, id}: {color: string; id: number}) => {
+  const isSafeSpot = useMemo(() => SafeSpots.includes(id), [id]);
+  const isStarSpot = useMemo(() => StarSpots.includes(id), [id]);
+  const isArrowSpot = useMemo(() => ArrowSpot.includes(id), [id]);
+
   return (
     <View
       className="w-full h-full border-[0.4px] items-center justify-center"
-      style={tw`bg-[${color}]`}>
+      style={tw`bg-[${isSafeSpot! ? color : 'white'}]`}>
+      {isStarSpot && <StarIcon size={20} color={'grey'} />}
+      {isArrowSpot && (
+        <ArrowRightIcon
+          size={RFValue(12)}
+          style={{
+            transform: [
+              {
+                rotate:
+                  id === 38
+                    ? '180deg'
+                    : id === 25
+                    ? '90deg'
+                    : id === 51
+                    ? '-90deg'
+                    : '0deg',
+              },
+            ],
+          }}
+          color={'grey'}
+        />
+      )}
       <View className="absolute top-0 left-0 right-0 bottom-0 z-[99]">
         {/* <Pile
           cell={true}
@@ -18,7 +44,6 @@ const Cell = ({color, id}: {color: string; id: number}) => {
           color={Colors.green}
         /> */}
       </View>
-      <Text>{id}</Text>
     </View>
   );
 };
